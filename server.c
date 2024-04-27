64ccc0067da5a4251bc6c982078d03a4aa6fb2a6
Kimberly Fong <kimberlyfong521@gmail.com>
2024-04-26 18:40:38 -0700

    new thing

--- server.c
+++ server.c
@@ -1,191 +1,104 @@
-#include <stdarg.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <getopt.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-/**
- * Project 1 starter code
- * All parts needed to be changed/added are marked with TODO
- */
+#define PORT 8081
+#define BUF_SIZE 1024
 
-#define BUFFER_SIZE 1024
-#define DEFAULT_SERVER_PORT 8081
-#define DEFAULT_REMOTE_HOST "131.179.176.34"
-#define DEFAULT_REMOTE_PORT 5001
+void send_file(int client_socket, const char *filename) {
+    char buffer[BUF_SIZE];
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        // File not found
+        dprintf(client_socket, "HTTP/1.1 404 Not Found\r\n\r\n");
+        return;
+    }
 
-struct server_app {
-    // Parameters of the server
-    // Local port of HTTP server
-    uint16_t server_port;
+    // Determine content type based on file extension
+    const char *extension = strrchr(filename, '.');
+    const char *content_type = "text/plain";
+    if (extension != NULL) {
+        if (strcmp(extension, ".html") == 0 || strcmp(extension, ".txt") == 0) {
+            content_type = "text/html";
+        } else if (strcmp(extension, ".jpg") == 0) {
+            content_type = "image/jpeg";
+        }
+    }
 
-    // Remote host and port of remote proxy
-    char *remote_host;
-    uint16_t remote_port;
-};
+    dprintf(client_socket, "HTTP/1.1 200 OK\r\n");
+    dprintf(client_socket, "Content-Type: %s\r\n\r\n", content_type);
 
-// The following function is implemented for you and doesn't need
-// to be change
-void parse_args(int argc, char *argv[], struct server_app *app);
+    size_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, BUF_SIZE, file)) > 0) {
+        write(client_socket, buffer, bytes_read);
+    }
 
-// The following functions need to be updated
-void handle_request(struct server_app *app, int client_socket);
-void serve_local_file(int client_socket, const char *path);
-void proxy_remote_file(struct server_app *app, int client_socket, const char *path);
+    fclose(file);
+}
 
-// The main function is provided and no change is needed
-int main(int argc, char *argv[])
-{
-    struct server_app app;
+int main() {
     int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len;
-    int ret;
+    socklen_t client_addr_len = sizeof(client_addr);
+    char request[BUF_SIZE];
 
-    parse_args(argc, argv, &app);
-
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        perror("socket failed");
+    // Create server socket
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
+    // Bind server socket to port
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(app.server_port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORT);
 
-    // The following allows the program to immediately bind to the port in case
-    // previous run exits recently
-    int optval = 1;
-    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-
-    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("bind failed");
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Binding failed");
+        close(server_socket);
         exit(EXIT_FAILURE);
     }
 
+    // Listen for incoming connections
     if (listen(server_socket, 10) == -1) {
-        perror("listen failed");
+        perror("Listening failed");
+        close(server_socket);
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d\n", app.server_port);
+    printf("Server listening on port %d...\n", PORT);
 
     while (1) {
-        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
-        if (client_socket == -1) {
-            perror("accept failed");
-            continue;
+        // Accept incoming connection
+        if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len)) == -1) {
+            perror("Accepting connection failed");
+            close(server_socket);
+            exit(EXIT_FAILURE);
         }
-        
-        printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        handle_request(&app, client_socket);
+
+        // Receive HTTP request
+        recv(client_socket, request, BUF_SIZE, 0);
+        printf("Received request: %s\n", request);
+
+        // Extract filename from request
+        char filename[BUF_SIZE];
+        sscanf(request, "GET /%s ", filename);
+
+        // Send requested file
+        send_file(client_socket, filename);
+
+        // Close client socket
         close(client_socket);
     }
 
+    // Close server socket
     close(server_socket);
+
     return 0;
-}
-
-void parse_args(int argc, char *argv[], struct server_app *app)
-{
-    int opt;
-
-    app->server_port = DEFAULT_SERVER_PORT;
-    app->remote_host = NULL;
-    app->remote_port = DEFAULT_REMOTE_PORT;
-
-    while ((opt = getopt(argc, argv, "b:r:p:")) != -1) {
-        switch (opt) {
-        case 'b':
-            app->server_port = atoi(optarg);
-            break;
-        case 'r':
-            app->remote_host = strdup(optarg);
-            break;
-        case 'p':
-            app->remote_port = atoi(optarg);
-            break;
-        default: /* Unrecognized parameter or "-?" */
-            fprintf(stderr, "Usage: server [-b local_port] [-r remote_host] [-p remote_port]\n");
-            exit(-1);
-            break;
-        }
-    }
-
-    if (app->remote_host == NULL) {
-        app->remote_host = strdup(DEFAULT_REMOTE_HOST);
-    }
-}
-
-void handle_request(struct server_app *app, int client_socket) {
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read;
-
-    // Read the request from HTTP client
-    // Note: This code is not ideal in the real world because it
-    // assumes that the request header is small enough and can be read
-    // once as a whole.
-    // However, the current version suffices for our testing.
-    bytes_read = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-    if (bytes_read <= 0) {
-        return;  // Connection closed or error
-    }
-
-    buffer[bytes_read] = '\0';
-    // copy buffer to a new string
-    char *request = malloc(strlen(buffer) + 1);
-    strcpy(request, buffer);
-
-    // TODO: Parse the header and extract essential fields, e.g. file name
-    // Hint: if the requested path is "/" (root), default to index.html
-    char file_name[] = "index.html";
-
-    // TODO: Implement proxy and call the function under condition
-    // specified in the spec
-    // if (need_proxy(...)) {
-    //    proxy_remote_file(app, client_socket, file_name);
-    // } else {
-    serve_local_file(client_socket, file_name);
-    //}
-}
-
-void serve_local_file(int client_socket, const char *path) {
-    // TODO: Properly implement serving of local files
-    // The following code returns a dummy response for all requests
-    // but it should give you a rough idea about what a proper response looks like
-    // What you need to do 
-    // (when the requested file exists):
-    // * Open the requested file
-    // * Build proper response headers (see details in the spec), and send them
-    // * Also send file content
-    // (When the requested file does not exist):
-    // * Generate a correct response
-
-    char response[] = "HTTP/1.0 200 OK\r\n"
-                      "Content-Type: text/plain; charset=UTF-8\r\n"
-                      "Content-Length: 15\r\n"
-                      "\r\n"
-                      "Sample response";
-
-    send(client_socket, response, strlen(response), 0);
-}
-
-void proxy_remote_file(struct server_app *app, int client_socket, const char *request) {
-    // TODO: Implement proxy request and replace the following code
-    // What's needed:
-    // * Connect to remote server (app->remote_server/app->remote_port)
-    // * Forward the original request to the remote server
-    // * Pass the response from remote server back
-    // Bonus:
-    // * When connection to the remote server fail, properly generate
-    // HTTP 502 "Bad Gateway" response
-
-    char response[] = "HTTP/1.0 501 Not Implemented\r\n\r\n";
-    send(client_socket, response, strlen(response), 0);
 }
